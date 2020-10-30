be4debb8692eb2900cb4d5080b8594f94a88e3e3
Gerónimo M <43770133+Bismarck-GM@users.noreply.github.com>
2020-10-30 13:20:50 -0300

    Add eat tab DOM creation. Add data to dishes.

--- src/modules/dishes.js
+++ src/modules/dishes.js
@@ -1,39 +1,39 @@
 const DISHES = [{
-  "image": "",
-  "title": "ASD",
-  "body": ""
+  "image": "https://www.cookwithmanali.com/wp-content/uploads/2014/11/Hakka-Noodles-1-500x500.jpg",
+  "title": "Noodles",
+  "body": "The most nutritious for the programmer"
 }, {
-  "image": "",
-  "title": "123",
-  "body": ""
+  "image": "https://images.clarin.com/2011/02/08/asado-de-tira-un-clasico___BkUoeV1Vg_340x340.jpg",
+  "title": "Asado",
+  "body": "The most nutritious for the programmer"
 }, {
-  "image": "",
-  "title": "ASqweD",
-  "body": ""
+  "image": "https://dishingouthealth.com/wp-content/uploads/2020/02/VegPaella-500x500.jpg",
+  "title": "Paella",
+  "body": "The most nutritious for the programmer... o para ti"
 }, {
-  "image": "",
-  "title": "qwert",
-  "body": ""
+  "image": "https://www.thespruceeats.com/thmb/u91gwhnYG4kP40OkswDDM1VQjQA=/3648x3648/smart/filters:no_upscale()/roasted-chicken-legs-521968290-5888fd773df78caebc737427.jpg",
+  "title": "Tasty Chicken",
+  "body": "The most nutritious for the programmer"
 }, {
-  "image": "",
-  "title": "",
-  "body": ""
+  "image": "https://www.moulinex.com.ar/wp-content/uploads/2017/06/Guiso-lentejas-1024x1024.jpg",
+  "title": "Lent's Stew",
+  "body": "The most nutritious for the programmer"
 }, {
-  "image": "",
-  "title": "",
-  "body": ""
+  "image": "https://www.thewholesomedish.com/wp-content/uploads/2019/04/The-Best-Classic-Hamburgers-550-500x500.jpg",
+  "title": "Hamburguer",
+  "body": "Keystone of nutritious food"
 }, {
-  "image": "",
-  "title": "",
-  "body": ""
+  "image": "https://pbs.twimg.com/profile_images/890804383427903488/EKEHI5EE.jpg",
+  "title": "Sandwich",
+  "body": "2nd keystone"
 }, {
-  "image": "",
-  "title": "",
-  "body": ""
+  "image": "https://cdn.telanganatoday.com/wp-content/uploads/2020/02/FRUITS-.jpg",
+  "title": "Fruits",
+  "body": "Stay healthy mate"
 }, {
-  "image": "",
-  "title": "",
-  "body": ""
+  "image": "https://cloudfront-us-east-1.images.arcpublishing.com/bonnier/OB7S7ZQJAVDRFGVXYHMD3FUS2Y.jpg",
+  "title": "More noodles",
+  "body": "Just in case"
 }];
 
 export {DISHES};
--- src/modules/eat.js
+++ src/modules/eat.js
@@ -1,34 +1,129 @@
 import { TABCONTENTS } from "./template";
 import { DISHES } from "./dishes";
 
-DISHES.forEach(dish => {
-    console.log(dish.title);
-});
-
 const LOADEAT = (() => {
-    let container = document.createElement('div');
-    container.classList.add('columns');
+
+    const half = Math.ceil(DISHES.length / 2);
+    const FIRSTHALF = DISHES.splice(0, half)
+    const SECONDHALF = DISHES.splice(-half)
+
+    let mainColumns = document.createElement('div');
+    mainColumns.classList.add('columns');
     let left = document.createElement('div');
-    left.classList.add('column', 'is-9', 'content');
-    let title = document.createElement('h1');
-    title.textContent = "Everything the average programmer needs";
-    let intro = document.createElement('p');
-    intro.textContent = "Welcome to Code Eats! Loop through our tabs to find what you're needed at this point... if not sure start from the next tab. Good luck in your journey programmer!"
-    left.appendChild(title, intro);
-    left.appendChild(intro);
-
+    left.classList.add('column', 'is-half', 'p-3');
     let right = document.createElement('div');
-    right.classList.add('column', 'is-3');
-    let figure = document.createElement('figure');
-    figure.classList.add('image', 'is-square');
-    let image = document.createElement('img');
-    image.setAttribute('src','https://lh3.googleusercontent.com/proxy/ALpTXTLwmlWC9ZndPnCxc0GWbVd6BmdE1D34DP3b8ixMg5I7TkD92aD7bluOCBDU3qw8LuFkg4urRWj8nUUjgWCHddTfUhI_MM9oVijAIAO25xVw3QWfajsqDuX3HE_e');
-    figure.appendChild(image);
-    right.appendChild(figure);
+    right.classList.add('column', 'is-half', 'p-3');
+    let innerLeftColumns = document.createElement('div');
+    innerLeftColumns.classList.add('columns');
+    let innerRightColumns = document.createElement('div');
+    innerRightColumns.classList.add('columns');
+    let innerLeftColumn = document.createElement('div');
+    innerLeftColumn.classList.add('column', 'is-three-quarters', 'p-3');
+    let innerRightColumn = document.createElement('div');
+    innerRightColumn.classList.add('column', 'is-three-quarters', 'p-3');
 
-    container.appendChild(left);
-    container.appendChild(right);
-    TABCONTENTS.appendChild(container);
+    console.log(FIRSTHALF[0])
+
+    FIRSTHALF.forEach(dish => {
+        let card = document.createElement('div');
+        card.classList.add('card', 'mt-3', 'mb-3');
+
+        let cardImage = document.createElement('div');
+        cardImage.classList.add('card-image');
+
+        let imageContainer = document.createElement('figure');
+        imageContainer.classList.add('image', 'is-1by1');
+
+        let image = document.createElement('img');
+        image.setAttribute('src', dish.image);
+        
+        imageContainer.appendChild(image);
+        cardImage.appendChild(imageContainer);
+
+        let cardContent = document.createElement('div');
+        cardContent.classList.add('card-content');
+
+        let mediaMain = document.createElement('div');
+        mediaMain.classList.add('media');
+
+        let mediaContent = document.createElement('div');
+        mediaContent.classList.add('media-content');
+
+        let mediaTitle = document.createElement('p');
+        mediaTitle.classList.add('title', 'is-4');
+        mediaTitle.textContent = dish.title;
+
+        mediaContent.appendChild(mediaTitle);
+        mediaMain.appendChild(mediaContent);
+
+        let content = document.createElement('div');
+        content.classList.add('content');
+        content.innerText = dish.body;
+
+        cardContent.appendChild(mediaMain);
+        cardContent.appendChild(content);
+
+        card.appendChild(cardImage);
+        card.appendChild(cardContent);
+
+        innerLeftColumn.appendChild(card);
+    });
+
+    SECONDHALF.forEach(dish => {
+        let card = document.createElement('div');
+        card.classList.add('card', 'mt-3', 'mb-3');
+
+        let cardImage = document.createElement('div');
+        cardImage.classList.add('card-image');
+
+        let imageContainer = document.createElement('figure');
+        imageContainer.classList.add('image', 'is-1by1');
+
+        let image = document.createElement('img');
+        image.setAttribute('src', dish.image);
+        
+        imageContainer.appendChild(image);
+        cardImage.appendChild(imageContainer);
+
+        let cardContent = document.createElement('div');
+        cardContent.classList.add('card-content');
+
+        let media = document.createElement('div');
+        media.classList.add('media');
+
+        let mediaContent = document.createElement('div');
+        mediaContent.classList.add('media-content');
+
+        let mediaTitle = document.createElement('p');
+        mediaTitle.classList.add('title', 'is-4');
+        mediaTitle.textContent = dish.title;
+
+        mediaContent.appendChild(mediaTitle);
+        media.appendChild(mediaContent);
+
+        let content = document.createElement('div');
+        content.classList.add('content');
+        content.innerText = dish.body;
+
+        cardContent.appendChild(media);
+        cardContent.appendChild(content);
+
+        card.appendChild(cardImage);
+        card.appendChild(cardContent);
+
+        innerRightColumn.appendChild(card);
+    });
+
+    innerLeftColumns.appendChild(innerLeftColumn);
+    innerRightColumns.appendChild(innerRightColumn);
+
+    left.appendChild(innerLeftColumns);
+    right.appendChild(innerRightColumns);
+
+    mainColumns.appendChild(left);
+    mainColumns.appendChild(right);
+
+    TABCONTENTS.appendChild(mainColumns);
 });
 
 export{LOADEAT};
